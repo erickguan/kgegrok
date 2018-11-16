edd4982f2a7010cc7d367fbb2a548be183b31c4e
Erick Guan <fantasticfears@gmail.com>
2018-11-16 12:14:45 +0100

    add cli parser

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import torch
 import data
-import kgekit
 import models
 from estimate import train_and_validate
 import logging
 import torch.optim as optim
 import visdom
 import numpy as np
+import sys
+import argparse
+from itertools import filterfalse
 from utils import report_gpu_info
 
 class Config(object):
@@ -19,7 +21,7 @@ class Config(object):
     num_workers = 2
     entity_embedding_dimension = 50
     margin = 1.0
-    epoches = 1
+    epoches = 1000
     alpha = 0.001
     lr_decay = 0.96
     weight_decay = 0.96
@@ -32,9 +34,40 @@ class Config(object):
     cudnn_benchmark = True
     logging_path = "logs"
     name = "TransE-YAGO3_10"
+    enable_cuda = True
 
-def cli():
-    config = Config()
+    @classmethod
+    def registered_options(cls):
+        """Returns a iterator"""
+        return filterfalse(lambda x: x.startswith('_') or type(cls.__dict__[x]) == classmethod, cls.__dict__)
+
+    @classmethod
+    def option_type(cls, key):
+        """Returns type class of given option"""
+        return type(cls.__dict__[key])
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(Config, cls).__new__(cls)
+        for k in cls.registered_options():
+            instance.__setattr__(k, cls.__dict__[k])
+        return instance
+
+    def __init__(self, args={}):
+        for k, v in args.items():
+            if v is not None:
+                self.__dict__[k] = v
+
+
+def cli(args):
+    parser = argparse.ArgumentParser()
+    for k in Config.registered_options():
+        parser.add_argument("--{}".format(k), type=Config.option_type(k))
+
+    parsed_args = parser.parse_args(args)
+    config = Config(vars(parsed_args))
+    config.enable_cuda = True if torch.cuda.is_available() and config.enable_cuda else False
+    print(config.__dict__)
+
     np.random.seed(10000)
     torch.manual_seed(20000)
     torch.cuda.manual_seed_all(2192)
@@ -43,8 +76,8 @@ def cli():
     model = train_and_validate(config, models.TransE, optim.Adam, visdom.Visdom(port=6006))
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     report_gpu_info()
 
-    cli()
+    cli(sys.argv[1:])
 
