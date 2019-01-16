8706fa033df52db7c940613d4591d8070ea77bd1
Erick Guan <fantasticfears@gmail.com>
2019-01-16 16:22:08 +0100

    put script into main module

--- scripts/grid_transe_fb15k237.py
+++ scripts/grid_transe_fb15k237.py
@@ -18,64 +18,64 @@ from kgegrok import evaluation
 from kgegrok import utils
 from kgegrok.stats import create_drawer
 
+if __name__ == '__main__':
+    grid = [{
+        'negative_entity': [1, 5, 10, 15],
+        'negative_relation': [0, 1, 5],
+        'entity_embedding_dimension': [30, 50, 100, 150, 200],
+        'alpha': [0.001, 0.005, 0.01, 0.1]
+    }
+    ]
 
-grid = [{
-    'negative_entity': [1, 5, 10, 15],
-    'negative_relation': [0, 1, 5],
-    'entity_embedding_dimension': [30, 50, 100, 150, 200],
-    'alpha': [0.001, 0.005, 0.01, 0.1]
-  }
-]
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
+                        level=logging.INFO)
+    utils.report_gpu_info()
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
-utils.report_gpu_info()
+    default_args = {
+        'data_dir': 'data/FB15k-237',
+        'triple_order': 'htr',
+        'delimiter': ' ',
+        'batch_size': 10000,
+        'num_workers': 10,
+        'num_evaluation_workers': 10,
+        'model': "TransE",
+        'optimizer': "SGD",
+        'margin': 1.0,
+        'epochs': 1000,
+        'lambda_': 0.001
+    }
+    config = utils.build_config_with_dict(default_args)
+    print(config.__dict__)
 
-default_args = {
-    'data_dir': 'data/FB15k-237',
-    'triple_order': 'htr',
-    'delimiter': ' ',
-    'batch_size': 10000,
-    'num_workers': 10,
-    'num_evaluation_workers': 10,
-    'model': "TransE",
-    'optimizer': "SGD",
-    'margin': 1.0,
-    'epochs': 1000,
-    'lambda_': 0.001
-}
-config = utils.build_config_with_dict(default_args)
-print(config.__dict__)
-
-triple_source = data.TripleSource(config.data_dir, config.triple_order,
-                                  config.triple_delimiter)
-model_class = utils.load_class_from_module(config.model, 'kgegrok.models',
-                                        'kgegrok.text_models')
+    triple_source = data.TripleSource(config.data_dir, config.triple_order,
+                                    config.triple_delimiter)
+    model_class = utils.load_class_from_module(config.model, 'kgegrok.models',
+                                            'kgegrok.text_models')
 
 
-with evaluation.validation_resource_manager('train_validate', config, triple_source) as pool:
-    for changed_config in ParameterGrid(grid):
-        d = {}
-        d.update(config.__dict__)
-        changed_config['name'] = "TransE-FB15k237-neg_e_{}-neg_r_{}-ent_dim_{}-alpha_{}".format(
-            changed_config['negative_entity'],
-            changed_config['negative_relation'],
-            changed_config['entity_embedding_dimension'],
-            changed_config['alpha'])
-        d.update(changed_config)
-        search_config = utils.build_config_with_dict(d)
+    with evaluation.validation_resource_manager('train_validate', config, triple_source) as pool:
+        for changed_config in ParameterGrid(grid):
+            d = {}
+            d.update(config.__dict__)
+            changed_config['name'] = "TransE-FB15k237-neg_e_{}-neg_r_{}-ent_dim_{}-alpha_{}".format(
+                changed_config['negative_entity'],
+                changed_config['negative_relation'],
+                changed_config['entity_embedding_dimension'],
+                changed_config['alpha'])
+            d.update(changed_config)
+            search_config = utils.build_config_with_dict(d)
 
-        utils.seed_modules(
-            config,
-            numpy_seed=10000,
-            torch_seed=20000,
-            torcu_cuda_seed_all=2192,
-            cuda_deterministic=True,
-            kgegrok_base_seed=30000,
-            cuda_benchmark=config.cudnn_benchmark)
+            utils.seed_modules(
+                config,
+                numpy_seed=10000,
+                torch_seed=20000,
+                torcu_cuda_seed_all=2192,
+                cuda_deterministic=True,
+                kgegrok_base_seed=30000,
+                cuda_benchmark=config.cudnn_benchmark)
 
-        optimizer_class = utils.load_class_from_module(config.optimizer,
-                                                       'torch.optim')
-        model = estimate.train_and_validate(triple_source, search_config, model_class,
-                                            optimizer_class, pool, drawer=stats.create_drawer(config))
+            optimizer_class = utils.load_class_from_module(config.optimizer,
+                                                        'torch.optim')
+            model = estimate.train_and_validate(triple_source, search_config, model_class,
+                                                optimizer_class, pool, drawer=stats.create_drawer(config))
