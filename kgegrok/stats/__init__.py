20511eafed17e8395e233830f4d75705c6246b73
Erick Guan <fantasticfears@gmail.com>
2019-02-25 13:57:03 +0100

    remove base seed

--- kgegrok/stats/__init__.py
+++ kgegrok/stats/__init__.py
@@ -173,6 +173,9 @@ class ReportDrawer(object):
       print(content)
     return content
 
+  def log_to_filename(log_path):
+    self.drawer.log_to_filename(log_path)
+
   def dump_raw_data(self):
     """Dumps all raw data."""
 
